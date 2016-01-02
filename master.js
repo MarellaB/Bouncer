95cc87427090559c928fd0426731cb3d7addef21
= <=>
2016-01-02 00:26:57 -0500

    Added Platform
    Added functioning platform function. And minor tweaks to code
    presentation.

--- master.js
+++ master.js
@@ -3,14 +3,36 @@ var canv = document.getElementById('primeCanvas'),
     $    = canv.getContext('2d');
 
 //Elements of page to be modified
-var scoreEl = document.getElementById('score'),
+var scoreEl      = document.getElementById('score'),
     multiplierEl = document.getElementById('multipler');
 
 //Inner variables of game
-var score = 0,
+var score     = 0,
     multipler = 1,
-    ballPosX = 0,
-    ballPosY = 0;
+    ballPosX  = 0,
+    ballPosY  = 0;
+
+var platform = {
+  posX: 'left',
+  posY: 0,
+  render() {
+    $.fillStyle = "#f0f";
+    if (this.posX === 'left') {
+      $.fillRect(0,this.posY,50, 15);
+    } else if (this.posX === 'middle') {
+      $.fillRect(75,this.posY,50, 15);
+    } else if (this.posX === 'right') {
+      $.fillRect(150,this.posY,50, 15);
+    }
+  },
+  tick() {
+
+  }
+}
+
+var platforms = Array();
+
+platforms.push(platform)
 
 //All logic
 function tick() {
@@ -19,9 +41,12 @@ function tick() {
 
 //All rendering
 function draw() {
-
+  
+  for (var i=0; i<platforms.length; i++) {
+    platforms[i].render();
+  }
 }
 
 //Two loops
 setInterval(tick, 3.333333);
-setInterval(draw, 3.33333;
+setInterval(draw, 3.33333);
