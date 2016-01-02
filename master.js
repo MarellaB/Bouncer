8618b0c96c1094bb28f9d6faf767ea9aa045daad
= <=>
2016-01-02 00:05:35 -0500

    Initial Commit
    Added base files

---
+++ master.js
@@ -0,0 +1,27 @@
+//All rendering variables
+var canv = document.getElementById('primeCanvas'),
+    $    = canv.getContext('2d');
+
+//Elements of page to be modified
+var scoreEl = document.getElementById('score'),
+    multiplierEl = document.getElementById('multipler');
+
+//Inner variables of game
+var score = 0,
+    multipler = 1,
+    ballPosX = 0,
+    ballPosY = 0;
+
+//All logic
+function tick() {
+
+}
+
+//All rendering
+function draw() {
+
+}
+
+//Two loops
+setInterval(tick, 3.333333);
+setInterval(draw, 3.33333;
