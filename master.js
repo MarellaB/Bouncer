1fbe7a4040271076f24ce9541b40ea2976ee027e
Nipponkid <brown.5776@osu.edu>
2016-01-02 00:29:21 -0500

    Implement drawBall
    Implemented the function that draws the ball onto the canvas.

--- master.js
+++ master.js
@@ -9,19 +9,34 @@ var scoreEl = document.getElementById('score'),
 //Inner variables of game
 var score = 0,
     multipler = 1,
-    ballPosX = 0,
-    ballPosY = 0;
+    ballRadius = 10,
+    ballPosX = (canv.width / 2),
+    ballPosY = (canv.height - ballRadius);
 
 //All logic
 function tick() {
+  ballPosX += 0;
+  ballPosY += 0;
+}
 
+/**
+ * Draw the ball. Defaults to the bottom center of the screen.
+ */
+function drawBall() {
+  $.beginPath();
+  $.arc(ballPosX, ballPosY, ballRadius, 0, Math.PI * 2, false);
+  $.fillStyle = "#FF0000";
+  $.fill();
+  $.closePath();
 }
 
 //All rendering
 function draw() {
-
+  // Clear canvas
+  $.clearRect(0, 0, canv.width, canv.height);
+  drawBall();
 }
 
 //Two loops
 setInterval(tick, 3.333333);
-setInterval(draw, 3.33333;
+setInterval(draw, 3.333333);
