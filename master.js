baa1578aebde18e5593d2943a550a535f097d215
= <=>
2016-01-04 02:28:17 -0500

    I dont think I did anything, at all but change spaces...

--- master.js
+++ master.js
@@ -3,12 +3,13 @@ var canv = document.getElementById('primeCanvas'),
     $    = canv.getContext('2d');
 
 //Elements of page to be modified
-var scoreEl = document.getElementById('score'),
+var scoreEl      = document.getElementById('score'),
     multiplierEl = document.getElementById('multipler');
 
 //Inner variables of game
-var score = 0,
+var score     = 0,
     multipler = 1,
+    camera    = 100;
     ballRadius = 10,
     ballPosX = (canv.width / 2),
     ballPosY = (canv.height - ballRadius),
@@ -21,11 +22,11 @@ var platform = {
   render() {
     $.fillStyle = "#f0f";
     if (this.posX === 'left') {
-      $.fillRect(0,this.posY,50, 15);
+      $.fillRect(0,this.posY+canv.height-camera,50, 15);
     } else if (this.posX === 'middle') {
-      $.fillRect(75,this.posY,50, 15);
+      $.fillRect(75,this.posY+canv.height-camera,50, 15);
     } else if (this.posX === 'right') {
-      $.fillRect(150,this.posY,50, 15);
+      $.fillRect(150,this.posY+canv.height-camera,50, 15);
     }
   },
   tick() {
@@ -35,7 +36,7 @@ var platform = {
 
 var platforms = [];
 
-platforms.push(platform)
+platforms.push(platform);
 
 //All logic
 function tick() {
@@ -49,7 +50,6 @@ function tick() {
   if ((ballPosY + ballRadius) > canv.height || (ballPosY - ballRadius) < 0) {
     dy = -dy;
   }
-
 }
 
 /**
@@ -63,17 +63,23 @@ function drawBall() {
   $.closePath();
 }
 
+//All rendering
+function draw() {}
+
 /**
  * Draw the bricks.
  */
 function drawBricks() {
   for (var i=0; i<platforms.length; i++) {
     platforms[i].render();
+    console.log(platforms[i].posY);
   }
 }
 
 //All rendering
 function draw() {
+  $.fillStyle = "#000";
+  $.fillRect(0, 0, canv.width, canv.height);
   // Clear canvas
   $.clearRect(0, 0, canv.width, canv.height);
   drawBall();
